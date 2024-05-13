343fe136e2f0369408db9197d33accf0fc72d3f5
Damian Ciaszczyk <01187521@pw.edu.pl>
2024-05-13 22:20:44 +0200

    binary read

--- src/MazeSolverGUI.java
+++ src/MazeSolverGUI.java
@@ -1,16 +1,20 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.*;
 
 public class MazeSolverGUI extends JFrame {
-    private JButton loadTextButton, loadBinaryButton, findPathButton, selectStartButton;
+    private JButton loadTextButton, loadBinaryButton, findPathButton, selectStartButton, SaveLabirynthButton;
     private JPanel topPanel, bottomPanel, sidePanel, errorPanel;
     private JScrollPane scrollPane;
     private MazePanel mazePanel;
@@ -28,17 +32,20 @@ public class MazeSolverGUI extends JFrame {
         loadBinaryButton = new JButton("Wczytaj labirynt z pliku binarnego");
         findPathButton = new JButton("Znajdz najkrotsza sciezke");
         selectStartButton = new JButton("Wybierz punkt poczatkowy i koncowy");
+        SaveLabirynthButton = new JButton("Zapisz labirynt do pliku");
 
         loadTextButton.setFocusable(false);
         loadBinaryButton.setFocusable(false);
         findPathButton.setFocusable(false);
         selectStartButton.setFocusable(false);
+        SaveLabirynthButton.setFocusable(false);
 
         // wczytywanie cos robi
         loadTextButton.addActionListener(new LoadTextListener());
         loadBinaryButton.addActionListener(new LoadBinaryListener());
         findPathButton.addActionListener(new FindPathListener());
         selectStartButton.addActionListener(new SelectStartListener());
+        SaveLabirynthButton.addActionListener(new SaveLabirynthListener());
 
         JLabel pole = new JLabel("MENU:");
 
@@ -53,9 +60,10 @@ public class MazeSolverGUI extends JFrame {
         //guziki do pracy z labiryntem
         bottomPanel = new JPanel();
         bottomPanel.setLayout(new FlowLayout());
+        bottomPanel.setBackground(Color.LIGHT_GRAY);
         bottomPanel.add(findPathButton);
         bottomPanel.add(selectStartButton);
-        bottomPanel.setBackground(Color.LIGHT_GRAY);
+        bottomPanel.add(SaveLabirynthButton);
 
         // poczatkowy panel
         JPanel contentPanel = new JPanel(new BorderLayout());
@@ -86,6 +94,7 @@ public class MazeSolverGUI extends JFrame {
         //wygaszenie guzików
         findPathButton.setEnabled(false);
         selectStartButton.setEnabled(false);
+        SaveLabirynthButton.setEnabled(false);
 
         errorMessages = new ArrayList<>();
 
@@ -96,6 +105,8 @@ public class MazeSolverGUI extends JFrame {
     private class LoadTextListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             JFileChooser fileChooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("pliki tekstowe .txt", "txt");
+            fileChooser.setFileFilter(filter);
             int returnValue = fileChooser.showOpenDialog(null);
             if (returnValue == JFileChooser.APPROVE_OPTION) {
                 File selectedFile = fileChooser.getSelectedFile();
@@ -106,6 +117,7 @@ public class MazeSolverGUI extends JFrame {
                     //Włączamy guziki(beta)
                     findPathButton.setEnabled(true);
                     selectStartButton.setEnabled(true);
+                    SaveLabirynthButton.setEnabled(true);
                 } catch (IOException ex) {
                     displayErrorMessage("Nie udało się wczytać labiryntu z pliku: " + ex.getMessage());
                 }
@@ -117,16 +129,19 @@ public class MazeSolverGUI extends JFrame {
     private class LoadBinaryListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             JFileChooser fileChooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("pliki binarne .bin", "bin");
+            fileChooser.setFileFilter(filter);
             int returnValue = fileChooser.showOpenDialog(null);
             if (returnValue == JFileChooser.APPROVE_OPTION) {
                 File selectedFile = fileChooser.getSelectedFile();
                 try {
-                    loadMazeFromFile(selectedFile);
+                    loadMazeFromBinaryFile(selectedFile);
                     mazePanel.repaint();
                     adjustScrollPane();
                     // guziki
                     findPathButton.setEnabled(true);
                     selectStartButton.setEnabled(true);
+                    SaveLabirynthButton.setEnabled(true);
                 } catch (IOException ex) {
                     displayErrorMessage("Nie udało się wczytać labiryntu z pliku binarnego: " + ex.getMessage());
                 }
@@ -146,6 +161,14 @@ public class MazeSolverGUI extends JFrame {
     private class SelectStartListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             // na razie nie ma
+            displayErrorMessage("Niezaimplementowane: start-koniec");
+        }
+    }
+
+    //Listener do Zapisu
+    private class     SaveLabirynthListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            //WriteToFile(maze);
             displayErrorMessage("Niezaimplementowane: start-koniec");
         }
     }
@@ -175,6 +198,62 @@ public class MazeSolverGUI extends JFrame {
 
         //dynamiczny rozmiar panelu
         mazePanel.setMazeSize(numRows, numCols);
+    }
+
+    //Biedabinary wczytywanie
+    private void loadMazeFromBinaryFile(File file) throws IOException {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            //Nagłówek
+            int fileId = readInt(fileInputStream, 32);
+            int escape = readInt(fileInputStream, 8);
+            int columns = readInt(fileInputStream, 16);
+            int lines = readInt(fileInputStream, 16);
+            int entryX = readInt(fileInputStream, 16);
+            int entryY = readInt(fileInputStream, 16);
+            int exitX = readInt(fileInputStream, 16);
+            int exitY = readInt(fileInputStream, 16);
+            readInt(fileInputStream, 96); // Reserved
+            int counter = readInt(fileInputStream, 32);
+            int solutionOffset = readInt(fileInputStream, 32);
+            int separator = readInt(fileInputStream, 8);
+            int wall = readInt(fileInputStream, 8);
+            int path = readInt(fileInputStream, 8);
+            int numRows = 0;
+            int numCols = 0;
+            mazeArray = new char[lines][columns];
+            // czytanko w pętelce
+            while (fileInputStream.available() > 0) {
+                int separatorValue = readInt(fileInputStream, 8);
+                int value = readInt(fileInputStream, 8);
+                int count = readInt(fileInputStream, 8);
+                for(int i=0;i<=count;i++){
+                    if(value==wall){
+                        mazeArray[numRows][numCols]='X';
+                    } else {
+                        mazeArray[numRows][numCols]=' ';
+                    }
+                    numCols++;
+                    if(numCols==columns){
+                        numRows++;
+                        numCols=0;
+                    }
+                }
+            }
+            mazeArray[entryY-1][entryX-1]='P';
+            mazeArray[exitY-1][exitX-1]='K';
+            //dynamiczny rozmiar panelu
+            mazePanel.setMazeSize(lines, columns);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int readInt(InputStream inputStream, int numBits) throws IOException {
+        int result = 0;
+        for (int i = 0; i < numBits; i++) {
+            result |= (inputStream.read() & 0x01) << i;
+        }
+        return result;
     }
 
     //przestawia scrollbary
@@ -244,11 +323,22 @@ protected void paintComponent(Graphics g) {
 
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numCols; col++) {
-                if (mazeArray[row][col] == 'X') {
-                    g.setColor(Color.BLACK);
-                } else {
-                    g.setColor(Color.WHITE);
-                }
+                switch(mazeArray[row][col]) {
+                    case 'X':
+                        g.setColor(Color.BLACK);
+                      break;
+                    case ' ':
+                        g.setColor(Color.WHITE);
+                      break;
+                    case 'P':
+                        g.setColor(Color.GREEN);
+                    case 'K':
+                        g.setColor(Color.RED);
+                    case 'S':
+                        g.setColor(Color.YELLOW);
+                    default:
+                        displayErrorMessage("Nieznany znak w labiryncie");
+                  }
                 int x = offsetX + col * cellSize;
                 int y = offsetY + row * cellSize;
                 g.fillRect(x, y, cellSize, cellSize);
